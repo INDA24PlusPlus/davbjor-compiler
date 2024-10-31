92587e29a5a496b8ee78b288bb6a2fef68400fa9
David Björklund <divad.bjo@gmail.com>
2024-11-01 00:45:50 +0100

    Implemented full compile function

--- README.md
+++ README.md
@@ -4,3 +4,44 @@ Kod -> Lexer -> Parser -> AST-träd
 AST-träd -> constant-folding -> optimerat AST-träd
 
 AST-träd -> interpreter -> exekverad kod
+
+## BNF definition av DBLang
+```xml
+<program> ::= <block> | <block> <nl> <program>
+<block> ::= <ws> <block> 
+    | <while> 
+    | <if> 
+    | <declaration> 
+    | <assignment> 
+    | <output>
+
+<while> ::= "while" <ws> "(" <ws> <condition> <ws> ")" <ws> "{" <ws> <nl> <program> <nl> <ws> "}" <ws> ";"
+<if> ::= "if" <ws> "(" <ws> <condition> <ws> ")" <ws> "{" <nl> <program> <nl> <ws> "}" <ws> ";"
+
+<declaration> ::= "let " <ws> <identifier> <ws> "=" <ws> <statement> <ws> ";"
+<assignment> ::= <identifier> <ws> <assignor> <ws> <statement> <ws> ";"
+
+<output> ::= "print(" <ws> <statement> <ws> ")" <ws> ";" 
+
+<statement> ::= <condition> | <expression>
+<condition> ::= <ws> <expression> <ws> <comparator> <ws> <expression>
+<expression> ::= <value> 
+    | <expression> <ws> <operator> <ws> <expression> 
+    | "(" <ws> <expression> <ws> ")"
+
+<value> ::= <number> | <identifier>
+
+<identifier> ::= <letter> | <identifier> <letter>
+<number> ::= <digit> | <digit> <number> | <unary> <digit> | <unary> <digit> <number>
+<ws> ::= " " <ws> | ""
+<nl> ::= "\n" <nl> | ""
+<comparator> ::= "<" | ">" | "!=" | "==" | "<=" | ">="
+<assignor> ::= "=" | "+=" | "-=" | "*=" | "/="
+<operator> ::= "+" | "-" | "*" | "/"
+<unary> ::= "-"
+<digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+<letter> ::= "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z" | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" 
+```
+
+
+[https://bnfplayground.pauliankline.com...](https://bnfplayground.pauliankline.com/?bnf=%3Cprogram%3E%20%3A%3A%3D%20%3Cblock%3E%20%7C%20%3Cblock%3E%20%3Cnl%3E%20%3Cprogram%3E%0A%3Cblock%3E%20%3A%3A%3D%20%3Cws%3E%20%3Cblock%3E%20%0A%20%20%20%20%7C%20%3Cwhile%3E%20%0A%20%20%20%20%7C%20%3Cif%3E%20%0A%20%20%20%20%7C%20%3Cdeclaration%3E%20%0A%20%20%20%20%7C%20%3Cassignment%3E%20%0A%20%20%20%20%7C%20%3Coutput%3E%0A%0A%3Cwhile%3E%20%3A%3A%3D%20%22while%22%20%3Cws%3E%20%22(%22%20%3Cws%3E%20%3Ccondition%3E%20%3Cws%3E%20%22)%22%20%3Cws%3E%20%22%7B%22%20%3Cnl%3E%20%3Cprogram%3E%20%3Cnl%3E%20%3Cws%3E%20%22%7D%22%20%3Cws%3E%20%22%3B%22%0A%3Cif%3E%20%3A%3A%3D%20%22if%22%20%3Cws%3E%20%22(%22%20%3Cws%3E%20%3Ccondition%3E%20%3Cws%3E%20%22)%22%20%3Cws%3E%20%22%7B%22%20%3Cnl%3E%20%3Cprogram%3E%20%3Cnl%3E%20%3Cws%3E%20%22%7D%22%20%3Cws%3E%20%22%3B%22%0A%0A%3Cdeclaration%3E%20%3A%3A%3D%20%22let%20%22%20%3Cws%3E%20%3Cidentifier%3E%20%3Cws%3E%20%22%3D%22%20%3Cws%3E%20%3Cstatement%3E%20%3Cws%3E%20%22%3B%22%0A%3Cassignment%3E%20%3A%3A%3D%20%3Cidentifier%3E%20%3Cws%3E%20%3Cassignor%3E%20%3Cws%3E%20%3Cstatement%3E%20%3Cws%3E%20%22%3B%22%0A%0A%3Coutput%3E%20%3A%3A%3D%20%22print(%22%20%3Cws%3E%20%3Cstatement%3E%20%3Cws%3E%20%22)%22%20%3Cws%3E%20%22%3B%22%20%0A%0A%3Cstatement%3E%20%3A%3A%3D%20%3Ccondition%3E%20%7C%20%3Cexpression%3E%0A%3Ccondition%3E%20%3A%3A%3D%20%3Cws%3E%20%3Cexpression%3E%20%3Cws%3E%20%3Ccomparator%3E%20%3Cws%3E%20%3Cexpression%3E%0A%3Cexpression%3E%20%3A%3A%3D%20%3Cvalue%3E%20%0A%20%20%20%20%7C%20%3Cexpression%3E%20%3Cws%3E%20%3Coperator%3E%20%3Cws%3E%20%3Cexpression%3E%20%0A%20%20%20%20%7C%20%22(%22%20%3Cws%3E%20%3Cexpression%3E%20%3Cws%3E%20%22)%22%0A%0A%3Cvalue%3E%20%3A%3A%3D%20%3Cnumber%3E%20%7C%20%3Cidentifier%3E%0A%0A%3Cidentifier%3E%20%3A%3A%3D%20%3Cletter%3E%20%7C%20%3Cidentifier%3E%20%3Cletter%3E%0A%3Cnumber%3E%20%3A%3A%3D%20%3Cdigit%3E%20%7C%20%3Cdigit%3E%20%3Cnumber%3E%20%7C%20%3Cunary%3E%20%3Cdigit%3E%20%7C%20%3Cunary%3E%20%3Cdigit%3E%20%3Cnumber%3E%0A%3Cws%3E%20%3A%3A%3D%20%22%20%22%20%3Cws%3E%20%7C%20%22%20%22%0A%3Cnl%3E%20%3A%3A%3D%20%22%5Cn%22%20%3Cnl%3E%20%7C%20%22%5Cn%22%0A%3Ccomparator%3E%20%3A%3A%3D%20%22%3C%22%20%7C%20%22%3E%22%20%7C%20%22!%3D%22%20%7C%20%22%3D%3D%22%20%7C%20%22%3C%3D%22%20%7C%20%22%3E%3D%22%0A%3Cassignor%3E%20%3A%3A%3D%20%22%3D%22%20%7C%20%22%2B%3D%22%20%7C%20%22-%3D%22%20%7C%20%22*%3D%22%20%7C%20%22%2F%3D%22%0A%3Coperator%3E%20%3A%3A%3D%20%22%2B%22%20%7C%20%22-%22%20%7C%20%22*%22%20%7C%20%22%2F%22%0A%3Cunary%3E%20%3A%3A%3D%20%22-%22%0A%3Cdigit%3E%20%3A%3A%3D%20%220%22%20%7C%20%221%22%20%7C%20%222%22%20%7C%20%223%22%20%7C%20%224%22%20%7C%20%225%22%20%7C%20%226%22%20%7C%20%227%22%20%7C%20%228%22%20%7C%20%229%22%0A%3Cletter%3E%20%3A%3A%3D%20%22A%22%20%7C%20%22B%22%20%7C%20%22C%22%20%7C%20%22D%22%20%7C%20%22E%22%20%7C%20%22F%22%20%7C%20%22G%22%20%7C%20%22H%22%20%7C%20%22I%22%20%7C%20%22J%22%20%7C%20%22K%22%20%7C%20%22L%22%20%7C%20%22M%22%20%7C%20%22N%22%20%7C%20%22O%22%20%7C%20%22P%22%20%7C%20%22Q%22%20%7C%20%22R%22%20%7C%20%22S%22%20%7C%20%22T%22%20%7C%20%22U%22%20%7C%20%22V%22%20%7C%20%22W%22%20%7C%20%22X%22%20%7C%20%22Y%22%20%7C%20%22Z%22%20%7C%20%22a%22%20%7C%20%22b%22%20%7C%20%22c%22%20%7C%20%22d%22%20%7C%20%22e%22%20%7C%20%22f%22%20%7C%20%22g%22%20%7C%20%22h%22%20%7C%20%22i%22%20%7C%20%22j%22%20%7C%20%22k%22%20%7C%20%22l%22%20%7C%20%22m%22%20%7C%20%22n%22%20%7C%20%22o%22%20%7C%20%22p%22%20%7C%20%22q%22%20%7C%20%22r%22%20%7C%20%22s%22%20%7C%20%22t%22%20%7C%20%22u%22%20%7C%20%22v%22%20%7C%20%22w%22%20%7C%20%22x%22%20%7C%20%22y%22%20%7C%20%22z%22%20%0A&name=dblang)
--- davbjor-compiler/main.cpp
+++ davbjor-compiler/main.cpp
@@ -1,7 +1,3 @@
-/*
-#include "parser.cpp"
-*/
-
 #include <string>
 #include <vector>
 #include <map>
@@ -124,31 +120,30 @@ void debug_node(Node* node, int i = 0){
     return;
 }
 
-bool lexical(Node* node, std::set<std::string>* variables){
+// PERFORMS A RECURSIVE CHECK FOR UNDEFINED VARIABLES and REPEATED DEFINITIONS
+bool lexical(Node* node, std::set<std::string>* variables = new std::set<std::string>()){
     TokenKind kind = node->kind;
-    Node* left = node->left;
-    Node* right = node->right;
 
-    if (kind == NONE)return true;
-    
-    if (kind == NUMBER){
+    if (kind == NONE || kind == NUMBER)
         return true;
-    }
+
     if (kind == IDENTIFIER){
         // REQURIE VARIABLE TO BE DEFINED
         if (variables->find(node->name) == variables->end()){
-            std::cout << "VARIABLE " << node->name << " USED BUT NEVER DEFINED\n";
+            std::cout << "VARIABLE <" << node->name << "> USED BUT NEVER DEFINED\n";
             return false;
         }
         return true;
     }
+
     if(kind == LET){
         // ONLY ALLOW ONE DEFINITION OF A VARIABLE
         if (variables->find(node->left->name) != variables->end()){
-            std::cout << "VARIABLE " << node->left->name << " ALREADY DEFINED ONCE\n";
+            std::cout << "VARIABLE <" << node->left->name << "> ALREADY DEFINED ONCE\n";
             return false;
         }
 
+        // MAKE SURE VALUE TO BE ASSIGNED IS OK
         if (!lexical(node->right, variables))return false;
 
         variables->insert(node->left->name);
@@ -156,16 +151,17 @@ bool lexical(Node* node, std::set<std::string>* variables){
     }
 
     if(kind == SEMICOLON){
-        bool l = lexical(left, variables);
+        // PRINT ENTIRE LINE OF ERROR
+        bool l = lexical(node->left, variables);
 
         if (node->left->kind != SEMICOLON && !l){
             std::cout << "ERROR ON LINE:\n" << "\n";
             std::cout << node->left->print() << "\n";
         }
 
-        bool r = lexical(right, variables);
+        bool r = lexical(node->right, variables);
 
-        if (node->right->kind != SEMICOLON && !l){
+        if (node->right->kind != SEMICOLON && !r){
             std::cout << "ERROR ON LINE:\n" << "\n";
             std::cout << node->right->print() << "\n";
         }
@@ -173,122 +169,108 @@ bool lexical(Node* node, std::set<std::string>* variables){
         return l && r;
     }
 
-    return lexical(left, variables) && lexical(right, variables);
-}
-
-bool lexical_analasys(Node* node){
-    if (!lexical(node, new std::set<std::string>())){
-        std::cout << "FAILED LEXICAL!\n";
-        return 0;
-    }
-    return 1;
+    return lexical(node->left, variables) && lexical(node->right, variables);
 }
 
 
-Node* constant_folding(Node* node, int i = 0, bool comment = false){
+// EVALUATE ALL CONSTANT EXPRESSIONS DIRECTLY
+Node* constant_folding(Node* node){
     TokenKind kind = node->kind;
-    Node* left = node->left;
-    Node* right = node->right;
 
-    if (kind == NONE)return node;
+    if (kind == NONE || kind == NUMBER || kind == IDENTIFIER)
+        return node;
     
-    if (kind == NUMBER){
-        if(comment)std::cout << std::string(i*2, ' ') << "NUMBER: " << node->value << "\n"; 
-        return node;
-    }
-    if (kind == IDENTIFIER){
-        if(comment)std::cout << std::string(i*2, ' ') << "IDENTIFIER: " << node->name << "\n"; 
-        return node;
-    }
-    if (kind == SEMICOLON){
-        if(comment)std::cout << std::string(i*2, ' ') << "SEMICOLON" << "\n";
-        node->left = constant_folding(left);
-        node->right = constant_folding(right);
-        return node;
-    }
-    if (kind == IF){
-        if(comment)std::cout << std::string(i*2, ' ') << "IF" << "\n";
-        node->left = constant_folding(left, i+1);
-        /*if (node->left->kind == NUMBER && node->left->value == 0){
-            if(comment)std::cout << std::string(i*2, ' ') << "NEVER!\n";
-            return new Node();
-        }*/
-        if(comment)std::cout << std::string(i*2, ' ') << "THEN" << "\n";
-        node->right = constant_folding(right, i+1);
+    node->left = constant_folding(node->left); 
+    node->right = constant_folding(node->right); 
 
-        return node;
-    }
-    if(kind == WHILE){
-        if(comment)std::cout << std::string(i*2, ' ') << "WHILE" << "\n";
-        node->left = constant_folding(left, i+1);
-        if(comment)std::cout << std::string(i*2, ' ') << "DO" << "\n";
-        right = constant_folding(right, i+1);
+    if (is_arithmatic_kind(kind))
+        if(node->left->kind  == NUMBER && node->right->kind == NUMBER)
+            return new Node(NUMBER, interpret(node));
 
-        return node;
-    }
-    if(kind == ASSIGN || kind == LET){
-        if(comment)std::cout << std::string(i*2, ' ') << "ASSIGN" << "\n";
+    // 0 + X = X
+    if(kind == PLUS)
+        if(node->left->kind == NUMBER && node->left->value == 0)
+            return node->right;
 
-        if(comment)std::cout << std::string(i+1, ' ') << std::string(i+1, ' ') << left->name << "\n";
-        if(comment)std::cout << std::string(i*2, ' ') << "TO" << "\n";
-        node->right = constant_folding(right, i+1);
-        
-        return node;
-    }
-    if(kind == PRINT){
-        if(comment)std::cout << std::string(i*2, ' ') << "PRINT" << "\n";
-        node->left = constant_folding(left, i+1);
-
-        return node;
-    }
-
-    if(comment)std::cout << std::string(i*2, ' ');
-    if(comment)print_tokens(std::vector<Token>{Token(node->kind, "", 0)});
+    //X - 0 = X, X + 0 = X
+    if (kind == PLUS || kind == MINUS)
+        if(node->right->kind == NUMBER && node->right->value == 0)
+            return node->left;
     
-    if (is_arithmatic_kind(kind)) { 
-        if(comment)std::cout << std::string(i*2, ' ') << "IS ARTITM\n";
-        node->left = constant_folding(left, i+1); 
-        node->right = constant_folding(right, i+1); 
-    }
-    else return node;
 
-    if (node->left->kind == NUMBER && node->right->kind == NUMBER){
-        if(comment)std::cout << std::string(i*2, ' ') << "INTERPRET -> " << interpret(node) << "\n";
-        return new Node(NUMBER, interpret(node));
-    }
+    // 1 * X = X
+    if(kind == STAR)
+        if(node->left->kind == NUMBER && node->left->value == 1)
+            return node->right;
+
+    // X * 1 = X, X / 1 = X
+    if (kind == STAR || kind == DIV)
+        if(node->right->kind == NUMBER && node->right->value == 1)
+            return node->left;
+
+    // 0 * X = 0, X * 0 = 0
+    if (kind == STAR)
+        if(node->left->kind  == NUMBER && node->left->value == 0
+        || node->right->kind == NUMBER && node->right->value == 0)
+            return new Node(NUMBER, 0);
     
-    if(comment)std::cout << std::string(i*2, ' ') << "NOT INTERPRETED\n";
 
     return node;
 }
 
-
+// REMOVES CODE THAT CONSTANTLY NEVER WILL GET EXECUTED
 Node* dead_code(Node* node){
     TokenKind kind = node->kind;
 
-    if (kind == NONE)return node;
-    
-    if (kind == NUMBER)return node;
-    
-    if (kind == IDENTIFIER)return node;
+    if (kind == NONE || kind == NUMBER || kind == IDENTIFIER)
+        return node;
 
+    // REMOVE IF AND WHILE STATEMENTS THAT CONSTANTLY NEVER EXECUTES
     if (kind == IF || kind == WHILE){
         if (node->left->kind == NUMBER && node->left->value == 0)
             return new Node(NUMBER, 0);
     }
 
-
     node->left = dead_code(node->left);
     node->right = dead_code(node->right);
+
+    // REMOVE BLOCKS IN SEMICOLON THAT ARE CONSTANT 0 - ex. dead IFs and WHILEs
     if (kind == SEMICOLON){
-        if (node->left->kind == NUMBER && node->left->value == 0){
+        if (node->left->kind == NUMBER && node->left->value == 0)
             return node->right;
-        }
-        if (node->right->kind == NUMBER && node->right->value == 0){
+        if (node->right->kind == NUMBER && node->right->value == 0)
             return node->left;
-        }
     }
+
+    // REMOVE DEAD ASSIGN TO OWN IDENTIFIER 
+    if (kind == ASSIGN){
+        if (node->left->kind == IDENTIFIER && node->right->kind == IDENTIFIER)
+            if (node->left->name == node->right->name)
+                return new Node(NUMBER, 0);
+    }
+
+    // REMOVE IFs AND WHILEs with NO BODY
+    if (kind == IF || kind == WHILE){
+        if (node->right->kind == NUMBER && node->right->value == 0)
+            return new Node(NUMBER, 0);
+    }
+
     return node;
+}
+
+std::string compile(std::string code){
+    auto tokens = lex(code);
+    //print_tokens(tokens);
+
+    Node* expr = parse(&tokens);
+
+    if (!lexical(expr))return "";    
+    expr = constant_folding(expr);
+    expr = dead_code(expr);
+    //debug_node(expr);
+    
+
+    return expr->print();
 }
 
 
@@ -347,7 +329,6 @@ int main() {
 
     
     /*
-    */
     std::string code = R"""(
         // FIBONACCI
         let a = 0;
@@ -364,24 +345,19 @@ int main() {
 
     )""";
 
-    /*
+    */
     std::string code = R"""(
-    let rIC = (3 + 2 ) / 4;
-    let j = (16 / 4) * (1 + 2);
-    let WU = 32;
-    let MiW = 7;
-    if   (  ( 0    ) -  (  0   /  39 ) +   ( 93  /  02  ) !=   77 - j )  {
-        print(  j ) ;
-    }  ;
-    if  (  3 !=  075 )  {
-        while (  28 >= rIC  ) {
-            WU  /= MiW +   1 ;
-        } ;
+    let vg = 13;
+    let hT = 10;
+    let c = ((13 + 4) - (4 * 4 + 1)) + (1 - 1 + hT) * (2 - 1);
+    if (  vg   < -20 -  07-0   *   -6 ) {
+        vg += 1000;
+        hT +=   4 - 7-5 == -1   * 4  ;
     } ;
     )""";
 
     /*
-    IF L_PAREN L_PAREN NUMBER(86) DIV IDENTIFIER(ca) IDENTIFIER(a) DIV NUMBER(1) DIV IDENTIFIER(Fc) IDENTIFIER(c) NUMBER(9) STAR IDENTIFIER(JI) IDENTIFIER(I) NUMBER(3) R_PAREN DIV IDENTIFIER(Y) NUMBER(4) IDENTIFIER(L) NUMBER(5) GREATER_EQ L_PAREN NUMBER(3) R_PAREN R_PAREN L_CURLY IF L_PAREN NUMBER(5) DIV NUMBER(21) DIV NUMBER(6115) GREATER_EQ L_PAREN NUMBER(8) R_PAREN DIV NUMBER(3) R_PAREN L_CURLY IDENTIFIER(R) NUMBER(6) ASSIGN IDENTIFIER(D) NUMBER(7) STAR NUMBER(138) SEMICOLON IF L_PAREN L_PAREN IDENTIFIER(x) PLUS NUMBER(6) STAR IDENTIFIER(Y) STAR NUMBER(59) STAR NUMBER(17) R_PAREN EQ L_PAREN NUMBER(3) R_PAREN R_PAREN L_CURLY IF L_PAREN L_PAREN NUMBER(50) R_PAREN LESS_EQ L_PAREN NUMBER(5) R_PAREN R_PAREN L_CURLY IDENTIFIER(J) ASSIGN IDENTIFIER(Y) SEMICOLON WHILE L_PAREN L_PAREN L_PAREN IDENTIFIER(n) R_PAREN R_PAREN GREATER L_PAREN IDENTIFIER(H) R_PAREN R_PAREN L_CURLY WHILE L_PAREN L_PAREN L_PAREN IDENTIFIER(z) NUMBER(6) R_PAREN DIV IDENTIFIER(e) NUMBER(1) R_PAREN GREATER_EQ NUMBER(3) R_PAREN L_CURLY IDENTIFIER(I) ASSIGN IDENTIFIER(Y) DIV IDENTIFIER(t) SEMICOLON IF L_PAREN IDENTIFIER(f) NUMBER(3) MINUS NUMBER(79) NOT_EQ NUMBER(9128) R_PAREN L_CURLY IDENTIFIER(w) NUMBER(87) IDENTIFIER(X) ASSIGN NUMBER(86) MINUS NUMBER(39) MINUS IDENTIFIER(UU) IDENTIFIER(U) SEMICOLON R_CURLY SEMICOLON R_CURLY SEMICOLON R_CURLY SEMICOLON R_CURLY SEMICOLON R_CURLY SEMICOLON R_CURLY SEMICOLON R_CURLY SEMICOLON IF L_PAREN L_PAREN NUMBER(2) R_PAREN GREATER_EQ NUMBER(8) DIV NUMBER(1) PLUS IDENTIFIER(Y) R_PAREN L_CURLY IDENTIFIER(N) NUMBER(0) ASSIGN NUMBER(19) SEMICOLON PRINT L_PAREN IDENTIFIER(vpq) IDENTIFIER(q) R_PAREN SEMICOLON IDENTIFIER(wE) IDENTIFIER(E) NUMBER(6) IDENTIFIER(Y) ASSIGN L_PAREN NUMBER(3) R_PAREN SEMICOLON R_CURLY SEMICOLON NONE END_OF_FILE
+    /*
     */
     /*
     )-""";
@@ -389,84 +365,21 @@ int main() {
     
 
     /*
-
-
-
-<program> ::= <block> | <block> "\n" <program>
-<block> ::= <ws> <block> | <while> | <if> | <declaration> | <assignment> | <output>
-
-<while> ::= "while" <ws> "(" <ws> <condition> <ws> ")" <ws> "{\n" <program> "\n" <ws> "}" <ws> ";"
-<if> ::= "if" <ws> "(" <ws> <condition> <ws> ")" <ws> "{\n" <program> "\n" <ws> "}" <ws> ";"
-
-<declaration> ::= "let " <ws> <identifier> <ws> "=" <ws> <statement> <ws> ";"
-<assignment> ::= <identifier> <ws> <assignor> <ws> <statement> <ws> ";"
-
-<output> ::= "print(" <ws> <statement> <ws> ")" <ws> ";" 
-
-<statement> ::= <condition> | <expression>
-<condition> ::= <ws> <expression> <ws> <comparator> <ws> <expression>
-<expression> ::= <value> | <expression> <ws> <operator> <ws> <expression> | "(" <ws> <expression> <ws> ")"
-
-<value> ::= <number> | <identifier>
-
-<identifier> ::= <letter> | <identifier> <letter>
-<number> ::= <digit> | <digit> <number>
-<ws> ::= " " <ws> | " "
-<comparator> ::= "<" | ">" | "!=" | "==" | "<=" | ">="
-<assignor> ::= "=" | "+=" | "-=" | "*=" | "/="
-<operator> ::= "+" | "-" | "*" | "/"
-<digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
-<letter> ::= "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z" | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" 
-
-https://bnfplayground.pauliankline.com/?bnf=%3Cprogram%3E%20%3A%3A%3D%20%3Csyntax%3E%20%7C%20%3Csyntax%3E%20%22%5Cn%22%20%3Cprogram%3E%0A%3Csyntax%3E%20%3A%3A%3D%20%3Cws%3E%20%3Csyntax%3E%20%7C%20%3Cwhile%3E%20%7C%20%3Cif%3E%20%7C%20%3Cdeclaration%3E%20%7C%20%3Cassignment%3E%20%7C%20%3Coutput%3E%0A%0A%3Cwhile%3E%20%3A%3A%3D%20%22while%22%20%3Cws%3E%20%22(%22%20%3Cws%3E%20%3Ccondition%3E%20%3Cws%3E%20%22)%22%20%3Cws%3E%20%22%7B%5Cn%22%20%3Cprogram%3E%20%22%5Cn%22%20%3Cws%3E%20%22%7D%22%20%3Cws%3E%20%22%3B%22%0A%3Cif%3E%20%3A%3A%3D%20%22if%22%20%3Cws%3E%20%22(%22%20%3Cws%3E%20%3Ccondition%3E%20%3Cws%3E%20%22)%22%20%3Cws%3E%20%22%7B%5Cn%22%20%3Cprogram%3E%20%22%5Cn%22%20%3Cws%3E%20%22%7D%22%20%3Cws%3E%20%22%3B%22%0A%3Cdeclaration%3E%20%3A%3A%3D%20%22let%20%22%20%3Cws%3E%20%3Cvariable%3E%20%3Cws%3E%20%22%3D%22%20%3Cws%3E%20%3Cexpression%3E%20%3Cws%3E%20%22%3B%22%0A%0A%3Cassignment%3E%20%3A%3A%3D%20%3Cvariable%3E%20%3Cws%3E%20%3Cassignor%3E%20%3Cws%3E%20%3Cexpression%3E%20%3Cws%3E%20%22%3B%22%0A%3Coutput%3E%20%3A%3A%3D%20%22print(%22%20%3Cws%3E%20%3Cvariable%3E%20%3Cws%3E%20%22)%22%20%3Cws%3E%20%22%3B%22%20%0A%0A%0A%3Cws%3E%20%3A%3A%3D%20%22%20%22%20%3Cws%3E%20%7C%20%22%20%22%0A%0A%3Ccondition%3E%20%3A%3A%3D%20%3Cws%3E%20%3Cexpression%3E%20%3Cws%3E%20%3Ccomparator%3E%20%3Cws%3E%20%3Cexpression%3E%0A%3Cexpression%3E%20%3A%3A%3D%20%3Cvalue%3E%20%7C%20%3Cexpression%3E%20%3Cws%3E%20%3Coperator%3E%20%3Cws%3E%20%3Cexpression%3E%20%7C%20%22(%22%20%3Cws%3E%20%3Cexpression%3E%20%3Cws%3E%20%22)%22%0A%0A%3Cvalue%3E%20%3A%3A%3D%20%3Cnumber%3E%20%7C%20%3Cvariable%3E%0A%0A%0A%3Cvariable%3E%20%3A%3A%3D%20%3Cletter%3E%20%7C%20%3Cvariable%3E%20%3Cletter%3E%20%7C%20%3Cvariable%3E%20%3Cdigit%3E%0A%3Cnumber%3E%20%3A%3A%3D%20%3Cdigit%3E%20%7C%20%3Cdigit%3E%20%3Cnumber%3E%0A%3Ccomparator%3E%20%3A%3A%3D%20%22%3C%22%20%7C%20%22%3E%22%20%7C%20%22!%3D%22%20%7C%20%22%3D%3D%22%20%7C%20%22%3C%3D%22%20%7C%20%22%3E%3D%22%0A%3Cassignor%3E%20%3A%3A%3D%20%22%3D%22%20%7C%20%22%2B%3D%22%20%7C%20%22-%3D%22%20%7C%20%22*%3D%22%20%7C%20%22%2F%3D%22%0A%3Coperator%3E%20%3A%3A%3D%20%22%2B%22%20%7C%20%22-%22%20%7C%20%22*%22%20%7C%20%22%2F%22%0A%3Cdigit%3E%20%3A%3A%3D%20%220%22%20%7C%20%221%22%20%7C%20%222%22%20%7C%20%223%22%20%7C%20%224%22%20%7C%20%225%22%20%7C%20%226%22%20%7C%20%227%22%20%7C%20%228%22%20%7C%20%229%22%0A%3Cletter%3E%20%3A%3A%3D%20%22A%22%20%7C%20%22B%22%20%7C%20%22C%22%20%7C%20%22D%22%20%7C%20%22E%22%20%7C%20%22F%22%20%7C%20%22G%22%20%7C%20%22H%22%20%7C%20%22I%22%20%7C%20%22J%22%20%7C%20%22K%22%20%7C%20%22L%22%20%7C%20%22M%22%20%7C%20%22N%22%20%7C%20%22O%22%20%7C%20%22P%22%20%7C%20%22Q%22%20%7C%20%22R%22%20%7C%20%22S%22%20%7C%20%22T%22%20%7C%20%22U%22%20%7C%20%22V%22%20%7C%20%22W%22%20%7C%20%22X%22%20%7C%20%22Y%22%20%7C%20%22Z%22%20%7C%20%22a%22%20%7C%20%22b%22%20%7C%20%22c%22%20%7C%20%22d%22%20%7C%20%22e%22%20%7C%20%22f%22%20%7C%20%22g%22%20%7C%20%22h%22%20%7C%20%22i%22%20%7C%20%22j%22%20%7C%20%22k%22%20%7C%20%22l%22%20%7C%20%22m%22%20%7C%20%22n%22%20%7C%20%22o%22%20%7C%20%22p%22%20%7C%20%22q%22%20%7C%20%22r%22%20%7C%20%22s%22%20%7C%20%22t%22%20%7C%20%22u%22%20%7C%20%22v%22%20%7C%20%22w%22%20%7C%20%22x%22%20%7C%20%22y%22%20%7C%20%22z%22%20%0A&name=    */
-
-    std::cout << code << "\n";
-
     auto tokens = lex(code);
     //print_tokens(tokens);
 
     Node* expr = parse(&tokens);
 
-    if (!lexical_analasys(expr))return 0;
-    
+    if (!lexical(expr))return 0;    
     expr = constant_folding(expr);
-
     expr = dead_code(expr);
-
-    std::cout << expr->print() << "\n";
-
-    interpret(expr);
-    /*std::cout << expr->print() << "\n";
-
-    std::cout << "*****************\nSTART DEBUG\n";
-
-    debug_node(expr);
-    
-    std::cout << "END DEBUG\n-----------------\nFIX CODE\n---------------\n";
+    //debug_node(expr);
     */
 
-    
-    /*
-    std::cout << "END FIX\n******************\nSTART DEBUG\n---------------\n";
+    std::cout << "INPUT CODE -> \n\n" << code << "\n";
+    std::cout << "COMPILED INTO -> \n\n" << compile(code) << "\n";
 
-    debug_node(fixed);
-    */
-
-    /*
-    Node* expr2 = constfactor(expr);
-
-    std::cout << "*****************\nSTART DEBUG\n";
-
-    debug_node(expr);
-    
-    std::cout << "END DEBUG\n-----------------\n";
-
-    std::cout << expr2->print() << "\n";
-
-
-    Node* expr3 = constfactor(expr2);
-    */
-
+    //interpret(expr);
 
 
     return 0;
